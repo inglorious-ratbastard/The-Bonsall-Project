053db84715c7fe43c046a3e3f66b609f69cd95b5
inglorious-ratbastard <47618267+inglorious-ratbastard@users.noreply.github.com>
2023-10-06 00:51:33 +0000

    styling changes, css cleanup, implementation of color mode toggle

--- script.js
+++ script.js
@@ -49,3 +49,44 @@
         $('.container-m').toggleClass('open');
     })
 })
+
+// Get prefers-color-scheme media query
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  window.localStorage.setItem("isDarkMode", "true");
+}
+
+// When browser window loads, check dark mode
+window.onload = function() {
+  const isDarkMode = window.localStorage.getItem("isDarkMode") === "true";
+  
+  if (isDarkMode) {
+    document.body.style.background = "#E5E4E2"; /* "#000000"; */
+    document.getElementById("text").innerHTML = "Turn on the lights!";
+    document.getElementById("text").style.color = "#ffffff";
+    document.getElementById("checkbox").checked = false;    
+  } else {
+    document.body.style.background = "#ffffff"; 
+    document.getElementById("text").innerHTML = "Turn off the lights!";
+    document.getElementById("text").style.color = "#000000"; 
+    document.getElementById("checkbox").checked = true;
+  }
+}
+
+// Toggle dark mode
+function toggle() {
+  const isDarkMode = window.localStorage.getItem("isDarkMode") === "true";
+  
+  if (isDarkMode) {
+    window.localStorage.setItem("isDarkMode", "false");
+    document.body.style.background = "#B2BEB5"; /* "#ffffff"; */
+    document.getElementById("text").innerHTML = "Turn off the lights!";
+    document.getElementById("text").style.color = "#000000"; 
+    document.getElementById("checkbox").checked = true;
+  } else {
+    window.localStorage.setItem("isDarkMode", "true");
+    document.body.style.background = "#E5E4E2"; /* "#000000"; */
+    document.getElementById("text").innerHTML = "Turn on the lights!";
+    document.getElementById("text").style.color = "#ffffff";
+    document.getElementById("checkbox").checked = false;  
+  }
+}
