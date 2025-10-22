dc38f8903557ead0e5ef690bc4b16d1d914c28df
phoenixfiremind77 <phoenixfiremind77@gmail.com>
2025-10-22 23:55:37 +0000

    change overlay + refresh page glitch

--- replit.nix
+++ replit.nix
@@ -3,4 +3,24 @@
       pkgs.nodePackages.vscode-langservers-extracted
       pkgs.nodePackages.typescript-language-server
     ];
+    idx.previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [
+            "npm"
+            "run"
+            "start"
+            "--"
+            "--port"
+            "$PORT"
+            "--host"
+            "0.0.0.0"
+            "--disable-host-check"
+          ];
+          manager = "web";
+        };
+      };
+    };
   }
+  
